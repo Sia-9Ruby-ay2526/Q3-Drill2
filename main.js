65629bcf58a1857fe2a3b9308dc572040acc1daa
Maria Agatha Sia <sia.mb@obmontessori.edu.ph>
2026-03-15 10:56:48 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function calculateArea() {
+
+let base = document.getElementById("Goose").value;
+let height = document.getElementById("Carole").value;
+
+let area = (base * height) / 2;
+
+document.getElementById("Rooster").value = area;
+
+}
